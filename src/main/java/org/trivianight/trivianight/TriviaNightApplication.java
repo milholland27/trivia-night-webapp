a21c83ae70c76f024c48f64041d49cc78888a3bc
dmilhol <milholland@mac.com>
2019-06-19 15:12:35 -0500

    Initial commit

---
+++ src/main/java/org/trivianight/trivianight/TriviaNightApplication.java
@@ -0,0 +1,13 @@
+package org.trivianight.trivianight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaNightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaNightApplication.class, args);
+	}
+
+}
